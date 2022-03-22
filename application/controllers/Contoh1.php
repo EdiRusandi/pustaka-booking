5b381c9a3a0ebc8c511143e3a76165f8789bcaef
Edi Rusandi <edirusandi11@gmail.com>
2022-03-22 21:01:41 +0700

    pertama commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h>";
+        echo "Nama saya Edi Rusandi, saya tinggal di daerah jakarta utara, hoby - lagi memaksakan diri untuk mulai banyak membaca";
+    }
+}
